09cedea8aa76d708c5daece416eab77f2bf803ad
Joel Bartlett <joel@murbar.com>
2019-02-27 13:26:29 -0500

    Add remove button

--- friends/src/components/FriendsList.jsx
+++ friends/src/components/FriendsList.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const FriendsList = ({ data }) => {
+const FriendsList = ({ data, onRemove }) => {
   if (!data.length) return <div>Loading...</div>;
   return (
     <div className="friends-list">
       {data.map(f => (
         <div key={f.id} className="friends-list-item">
           {f.name}, age {f.age} - {f.email}
+          <button onClick={() => onRemove(f.id)}>X</button>
         </div>
       ))}
     </div>
