91746f5db3eaf8395e885c11d8b9152d10d32296
Joel Bartlett <joel@murbar.com>
2019-02-27 17:33:42 -0500

    Add update handler

--- friends/src/components/Friend.jsx
+++ friends/src/components/Friend.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const Friend = ({ data, onRemove }) => {
+const Friend = ({ data, onUpdate, onRemove }) => {
+  const handleUpdate = () => {
+    onUpdate(data.id);
+  };
+
   const handleRemove = () => {
     onRemove(data.id);
   };
@@ -8,6 +12,7 @@ const Friend = ({ data, onRemove }) => {
   return (
     <div className="friends-list-item">
       {data.name}, age {data.age} - {data.email}
+      <button onClick={handleUpdate}>Update</button>
       <button onClick={handleRemove}>Delete</button>
     </div>
   );
