68729192ec8ea794d88060c46f770df414dcab2e
Joel Bartlett <joel@murbar.com>
2019-02-27 10:53:08 -0500

    Display friends

--- friends/src/components/FriendsList.jsx
+++ friends/src/components/FriendsList.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 const FriendsList = ({ data }) => {
   if (!data.length) return <div>Loading...</div>;
   return (
-    <div>
-      list
-      <ul />
+    <div className="friends-list">
+      {data.map(f => (
+        <div key={f.id} className="friends-list-item">
+          {f.name}, age {f.age} - {f.email}
+        </div>
+      ))}
     </div>
   );
 };
