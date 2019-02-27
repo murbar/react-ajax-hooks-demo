d8280daa548c4f59d88c63963cae0faab4190e68
Joel Bartlett <joel@murbar.com>
2019-02-27 17:34:02 -0500

    Add update handler

--- friends/src/components/FriendsList.jsx
+++ friends/src/components/FriendsList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Friend from './Friend';
 
-const FriendsList = ({ data, onRemove }) => {
+const FriendsList = ({ data, onRemove, onUpdate }) => {
   if (!data.length) return <div>Loading...</div>;
   return (
     <div className="friends-list">
       {data.map(f => (
-        <Friend data={f} onRemove={onRemove} key={f.id} />
+        <Friend data={f} onRemove={onRemove} onUpdate={onUpdate} key={f.id} />
       ))}
     </div>
   );
