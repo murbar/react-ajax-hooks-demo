2a76116d6658cb2cda9ba1d53851cf3f422fc263
Joel Bartlett <joel@murbar.com>
2019-02-27 17:33:24 -0500

    Change var names

--- friends/src/App.jsx
+++ friends/src/App.jsx
@@ -6,11 +6,11 @@ import useApi from './hooks/useApi';
 
 const App = props => {
   const [friends, friendsApi] = useApi('http://localhost:5000/friends');
-  const [editingFriend, setEditingFriend] = useState(null);
+  const [editingData, setEditingData] = useState(null);
 
   const handleFormSubmit = friendData => {
     if (!friendData.id) {
-    friendsApi.create(friendData);
+      friendsApi.create(friendData);
     } else {
       friendsApi.update(friendData);
     }
@@ -21,7 +21,7 @@ const App = props => {
   };
 
   const populateFormForEditing = friendId => {
-    setEditingFriend(friends.find(f => f.id === friendId));
+    setEditingData(friends.find(f => f.id === friendId));
   };
 
   return (
@@ -32,7 +32,7 @@ const App = props => {
         onUpdate={id => populateFormForEditing(id)}
         onRemove={handleRemoveFriend}
       />
-      <FriendForm onSubmit={handleFormSubmit} initialValues={editingFriend} />
+      <FriendForm onSubmit={handleFormSubmit} initialValues={editingData} />
     </div>
   );
 };
