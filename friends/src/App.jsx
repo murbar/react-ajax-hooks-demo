0d66a8a781177cb1208b009275eb44cac32bed65
Joel Bartlett <joel@murbar.com>
2019-02-27 16:43:56 -0500

    Add form population for editing

--- friends/src/App.jsx
+++ friends/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FriendsList from './components/FriendsList';
 import FriendForm from './components/FriendForm';
 import './App.css';
@@ -6,6 +6,7 @@ import useApi from './hooks/useApi';
 
 const App = props => {
   const [friends, friendsApi] = useApi('http://localhost:5000/friends');
+  const [editingFriend, setEditingFriend] = useState(null);
 
   const handleFormSubmit = friendData => {
     friendsApi.create(friendData);
@@ -15,11 +16,19 @@ const App = props => {
     friendsApi.delete(friendId);
   };
 
+  const populateFormForEditing = friendId => {
+    setEditingFriend(friends.find(f => f.id === friendId));
+  };
+
   return (
     <div className="App">
       <h1>Friends</h1>
-      <FriendsList data={friends} onRemove={handleRemoveFriend} />
-      <FriendForm onSubmit={handleFormSubmit} />
+      <FriendsList
+        data={friends}
+        onUpdate={id => populateFormForEditing(id)}
+        onRemove={handleRemoveFriend}
+      />
+      <FriendForm onSubmit={handleFormSubmit} initialValues={editingFriend} />
     </div>
   );
 };
