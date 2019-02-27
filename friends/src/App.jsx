17bde3426f6763ae69783e89c8db3a6b8f42cdaa
Joel Bartlett <joel@murbar.com>
2019-02-27 12:47:18 -0500

    Convert component to function

--- friends/src/App.jsx
+++ friends/src/App.jsx
@@ -1,37 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import FriendsList from './components/FriendsList';
+import AddFriendForm from './components/AddFriendForm';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      friends: []
-    };
-  }
+const App = props => {
+  const [friends, setFriends] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
+    console.log('mounted and loading data');
     axios
       .get('http://localhost:5000/friends')
       .then(res => {
-        this.setState({
-          friends: res.data
-        });
+        setFriends(res.data);
       })
       .catch(err => console.log(err));
-  }
+  }, []);
 
-  render() {
-    const { friends } = this.state;
-
-    return (
-      <div className="App">
-        <h1>Friends</h1>
-        <FriendsList data={friends} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1>Friends</h1>
+      <FriendsList data={friends} />
+      <AddFriendForm />
+    </div>
+  );
+};
 
 export default App;
